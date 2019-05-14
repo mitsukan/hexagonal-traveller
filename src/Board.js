4abcfaf606614ee7c4e40311451347e1a8300c26
mitsukan <jim_ip20@hotmail.com>
2019-05-14 12:06:19 +0100

    Adding Tiles to Board

--- src/Board.js
+++ src/Board.js
@@ -1,3 +1,10 @@
-class Board{
+class Board extends Tile {
+  constructor(array) {
+    super();
+    this._array = [[new Tile(), new Tile()],[new Tile(), new Tile(), new Tile()],[new Tile(), new Tile()]];
+  }
 
+  get array() {
+    return this._array;
+  }
 }
