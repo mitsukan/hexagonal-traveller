c2a322e0fc14ccfaa441be7b23ab5736544c06f6
vrrgandham <venkat.gandham@gmail.com>
2019-05-15 12:01:44 +0100

    Added randomstart function

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,38 @@
 class Board{
-  constructor(array) {
+  constructor() {
     this._array = [[new Tile(), new Tile()],[new Tile(), new Tile(), new Tile()],[new Tile(), new Tile()]];
+    this._randomStart = new Array(2);
   }
 
   get array() {
     return this._array;
   }
 
-  // randomStart(){}
+  // get randomStart(){
+  //   return this._randomStart;
+  // }
+
+
 
   rollFirstIndex() {
-    return Math.floor(Math.random()*(2-0+1));
+
+    this._randomStart[0]=Math.floor(Math.random()*(2-0+1));
+
   }
+  rollSecondIndex(){
+    if (this._randomStart[0] === 0 ||this._randomStart[0] === 2){
+        this._randomStart[1]=Math.floor(Math.random()*(1-0+1));
+      }    else if (this._randomStart[0] ===1) {
+        this._randomStart[1]=Math.floor(Math.random()*(2-0+1));
+      }
+
+  }
+  randomStart(){
+this.rollFirstIndex();
+this.rollSecondIndex();
+
+  }
+
+
+
 }
