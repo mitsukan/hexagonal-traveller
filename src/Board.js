825d1aae9acaf2095c586ee7b4fd411f0b76aa99
mitsukan <jim_ip20@hotmail.com>
2019-05-15 13:36:11 +0100

    Cleaning up formatting for Board

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 class Board{
   constructor() {
     this._array = [[new Tile(), new Tile()],[new Tile(), new Tile(), new Tile()],[new Tile(), new Tile()]];
-    this._randomStart = new Array(2);
+    this._rS = new Array(2);
   }
 
   get array() {
@@ -12,27 +12,21 @@ class Board{
   //   return this._randomStart;
   // }
 
-
+  randomStart(){
+    this.rollFirstIndex();
+    this.rollSecondIndex();
+  }
 
   rollFirstIndex() {
-
-    this._randomStart[0]=Math.floor(Math.random()*(2-0+1));
-
+    this._rS[0]=Math.floor(Math.random()*(2-0+1));
   }
+
   rollSecondIndex(){
-    if (this._randomStart[0] === 0 ||this._randomStart[0] === 2){
-        this._randomStart[1]=Math.floor(Math.random()*(1-0+1));
-      }    else if (this._randomStart[0] ===1) {
-        this._randomStart[1]=Math.floor(Math.random()*(2-0+1));
-      }
-
+    if(this._rS[0] === 0 ||this._rS[0] === 2) {
+      this._rS[1]=Math.floor(Math.random()*(1-0+1));
+    } else if(this._rS[0] ===1) {
+      this._rS[1]=Math.floor(Math.random()*(2-0+1));
+    }
   }
-  randomStart(){
-this.rollFirstIndex();
-this.rollSecondIndex();
-
-  }
-
-
 
 }
